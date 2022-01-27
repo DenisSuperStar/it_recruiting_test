5e1127c39e15d7d0da54f956628941b6de9ee99a
Denis <den.shklyarskij@bk.ru>
2022-01-28 00:25:41 +0300

    moved routers in routes folder

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import UserController from "./controllers/userController";
-
 
 const app = express();
 const PORT: number = 8000;
@@ -10,38 +8,6 @@ app.set("View engine", "ejs");
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Express + type script server!");
-});
-
-let userController = new UserController(
-  "register",
-  "Моментальная регистрация!"
-);
-const { renderAccessForm, userRegisterProcessing } = userController;
-
-app.get("/register", renderAccessForm);
-app.post("/register", userRegisterProcessing);
-
-userController = new UserController("login", "Войти в приложение.");
-const { userLoginProcessing } = userController;
-
-app.get("/login", userController.renderAccessForm);
-
-app.post("/login", userLoginProcessing);
-
-app.get("/load-photos", (req: any, res: any) => {
-  res.send("Hello from load photos");
-});
-
-app.post("/load-photos", (req: any, res: any) => {
-  res.send("Hello from post load photos");
-});
-
-app.get("/get-photos", (req: any, res: any) => {
-  res.send("Hello from get photos");
-});
-
-app.post("/get-photos", (req: any, res: any) => {
-  res.send("Hello from post get photos");
 });
 
 app.get("/delete-photo", (req: any, res: any) => {
