f472a4860dd63ec668b480753b0e12112c6bc491
Denis <den.shklyarskij@bk.ru>
2022-01-30 22:07:26 +0300

    interface re-import

--- routes/users/autorization.ts
+++ routes/users/autorization.ts
@@ -1,7 +1,7 @@
-import View from "../../libs/view.interface";
+import IView from "../../libs/view.interface";
 import UserAutorizationController from "../../controllers/userAutorizeController";
 
-const view: View = { name: 'login', title: 'Войти в приложение.' }
+const view: IView = { name: 'login', title: 'Войти в приложение.' }
 const UserAutorize: UserAutorizationController = new UserAutorizationController(view.name, view.title);
 
 export default UserAutorize;
