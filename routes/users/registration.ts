4ef40e5a0d8800057ea0ba68cc9d9586688c2c58
Denis <den.shklyarskij@bk.ru>
2022-01-30 22:04:49 +0300

    interface re-import

--- routes/users/registration.ts
+++ routes/users/registration.ts
@@ -1,7 +1,7 @@
-import View from "../../libs/view.interface";
+import IView from "../../libs/view.interface";
 import UserRegistrationController from "../../controllers/userRegisterController";
 
-const view: View = { name: 'register', title: 'Моментальная регистрация!' }
+const view: IView = { name: 'register', title: 'Моментальная регистрация!' }
 const UserRegister: UserRegistrationController = new UserRegistrationController(view.name, view.title);
 
 export default UserRegister;
