0341e5948cd2070d909beb250df7668ce8078666
Denis <den.shklyarskij@bk.ru>
2022-02-04 15:11:11 +0300

    updated the code of App controller

--- index.ts
+++ index.ts
@@ -1,45 +1,66 @@
 import express from "express";
+import { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import { connect } from "mongoose";
 
-class Setting {
+class App {
+  public readonly PORT;
   private readonly app;
-  private readonly port: number;
-  public readonly engine: string = "ejs";
-  private readonly controllers: any[];
-  private readonly actions: any[];
+  private readonly engine;
+  private readonly controllers;
 
-  constructor(ctrls: any[], actions: any[], launchPort: number) {
-    this.port = launchPort;
-    this.controllers = ctrls;
-    this.actions = actions;
+  constructor(ctrls: any, engine: string, launchPort: number) {
     this.app = express();
-    this.initializeMiddleware();
-    this.initializeViewDirectory();
-    this.initializeEngine();
-    this.initializeControllers();
-    this.initializeActions();
+    this.engine = engine;
+    this.PORT = launchPort;
+    this.controllers = ctrls;
+    this.initMiddleware();
+    this.initViews();
+    this.initEngine();
+    this.initControllers();
   }
 
-  private initializeMiddleware(): void {
-    const { json, urlencoded } = express;
+  private initMiddleware(): void {
     this.app.use(json());
     this.app.use(urlencoded({ extended: false }));
     this.app.use(cookieParser());
   }
 
-  private initializeViewDirectory(): void {
+  private initViews(): void {
     this.app.set("views", "./views");
   }
 
-  private initializeEngine(): void {
+  private initEngine(): void {
     this.app.set("view engine", this.engine);
   }
 
-  private initializeControllers(): void {
-    this.controllers.forEach((controller) => {
-      controller.initializedRoutes();
+  private initControllers(): void {
+    this.controllers.forEach((controller: any) => {
+      controller.initializeRoutes();
     });
+  }
+
+  public launchApp(): void {
+    const DB_NAME = "strong_app";
+    connect(`mongodb://localhost:27017/${DB_NAME}`).then(() => {
+      console.log(`Server is running at https://localhost:${this.PORT}`);
+    });
+  }
+}
+
+export default App;
+/*
+
+class Setting {
+  
+  private readonly actions: any[];
+
+  constructor(ctrls: any[], actions: any[], launchPort: number) {
+    
+  
+    
+   
+    this.initializeActions();
   }
 
   private initializeActions(): void {
@@ -49,15 +70,6 @@ class Setting {
       this.app.use(serverErrorHandling);
     });
   }
-
-  public launcApp(): void {
-    const DB_NAME = "strong_app";
-    connect(`mongodb://localhost:27017/${DB_NAME}`).then(() => {
-      this.app.listen(this.port, () => {
-        console.log(`Server is running at https://localhost:${this.port}`);
-      });
-    });
-  }
 }
 
-export default Setting;
+export default Setting;*/
