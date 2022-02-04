57d723b8b5b673da3d84cef6840e6f1ae5edaf25
Denis <den.shklyarskij@bk.ru>
2022-02-04 14:23:48 +0300

    added new interfaces

--- interfaces/photoDocument.interface.ts
+++ interfaces/photoDocument.interface.ts
@@ -1,12 +1,12 @@
 import { Document } from "mongoose";
 
 interface IPhotoDocument extends Document {
-    albumId: number;
-    id: number;
-    title: string;
-    url: string;
-    thumbnailUrl: string;
-    owner: string;
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+  owner: string;
 }
 
 export default IPhotoDocument;
--- interfaces/userDocument.interface.ts
+++ interfaces/userDocument.interface.ts
@@ -1,10 +1,10 @@
 import { Document } from "mongoose";
 
 interface IUserDocument extends Document {
-    login: string;
-    email: string;
-    password: string;
-    token: string;
+  login: string;
+  email: string;
+  password: string;
+  token: string;
 }
 
 export default IUserDocument;
