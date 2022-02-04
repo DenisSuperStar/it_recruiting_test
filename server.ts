aea21829e040e8365029a3b7bbef25b069d28a92
Denis <den.shklyarskij@bk.ru>
2022-02-04 15:13:06 +0300

    updated code app launch file

--- server.ts
+++ server.ts
@@ -1,17 +1,37 @@
-import Setting from ".";
-import UserAutorizationController from "./controllers/userAutorizeController";
+import App from ".";
+
 import UserRegistrationController from "./controllers/userRegisterController";
+import UserAutorizationController from "./controllers/userAutorizeController";
 import LoadPhotosController from "./controllers/loadPhotosController";
+import GetPhotosController from "./controllers/getPhotosController";
+import DeletePhotoController from "./controllers/deletePhotoController";
+import DeleteAlbumIdController from "./controllers/deleteAlbumController";
+
+const PORT: number = 8080;
+const userRegister: UserRegistrationController =
+  new UserRegistrationController(); // ! передать какие-то классы
+const userAutorize: UserAutorizationController =
+  new UserAutorizationController(); // ! передать какие-то классы
+const loadPhotos: LoadPhotosController = new LoadPhotosController();
+const getPhotos: GetPhotosController = new GetPhotosController();
+const deletePhotos: DeletePhotoController = new DeletePhotoController();
+const deleteAlbum: DeleteAlbumIdController = new DeleteAlbumIdController();
+
+const controllers = [];
+controllers.push(userRegister);
+controllers.push(userAutorize);
+controllers.push(loadPhotos);
+controllers.push(getPhotos);
+controllers.push(deletePhotos);
+controllers.push(deleteAlbum);
+
+const app: App = new App(controllers, "ejs", PORT);
+const { launchApp } = app;
+
+launchApp();
+/*
 import ServerError from "./libs/serverError";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
-
-const PORT: number = 8000;
-
-const userAutorize: UserAutorizationController =
-  new UserAutorizationController();
-const userRegister: UserRegistrationController =
-  new UserRegistrationController();
-const loadPhotos: LoadPhotosController = new LoadPhotosController();
 
 const notFoundErrorProcess: ServerError = new ServerError(
   ReasonPhrases.NOT_FOUND,
@@ -22,17 +42,9 @@ const internalErrorProcess: ServerError = new ServerError(
   StatusCodes.INTERNAL_SERVER_ERROR
 );
 
-const controllers: any[] = [];
 const actions: any[] = [];
 
-controllers.push(userAutorize);
-controllers.push(userRegister);
-controllers.push(loadPhotos);
 
 actions.push(notFoundErrorProcess);
 actions.push(internalErrorProcess);
-
-const app: Setting = new Setting(controllers, actions, PORT);
-const { launcApp } = app;
-
-launcApp();
+*/
