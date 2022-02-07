74c853d9bd2173c8833123716f6ebe74d57db65a
Denis <den.shklyarskij@bk.ru>
2022-02-07 23:33:29 +0300

    added update many mongoose function with eq filter

---
+++ controllers/changeAlbumTitleController.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import IError from "../interfaces/error.interface"
+import Album from "../models/albums";
+
+class ChangeAlbumClassController {
+    private readonly path: string = '/change-album-title/:albumid/:new_album_name';
+    private readonly app;
+    private readonly unAutorize: IError;
+
+    constructor(unAuth: IError) {
+        this.app = express();
+        this.unAutorize = unAuth; // { name: ReasonPhrases.UNAUTORIZE, status: StatusCodes.UNAUTORIZE }
+    }
+
+    public initRoutes(): void {
+        this.app.get(this.path, this.changeAlbumTitle);
+    }
+
+    private async changeAlbumTitle(req: express.Request, res: express.Response): Promise<void> {
+        const { albumid, new_album_name } = req.params;
+        const { jwtToken } = req;
+
+        if (jwtToken) {
+            await Album.updateMany({ id: { $eq: albumid }}, { title: new_album_name });
+        } else {
+            res.json({
+              error: this.unAutorize.name,
+              statusCode: this.unAutorize.status  
+            });
+        }
+    }
+}
+
+export default ChangeAlbumClassController;
