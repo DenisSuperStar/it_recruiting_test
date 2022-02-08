4f2c63ca2ca8fee7cd28f7fda7739f71a89a6489
Denis <den.shklyarskij@bk.ru>
2022-02-08 03:40:59 +0300

    moved files from support to config

---
+++ support/views.ts
@@ -0,0 +1,6 @@
+const viewList =  {
+    register : { name: 'register', title: 'Моментальная регистрация!' },
+    autorize: { name: 'login', title: 'Войти в приложение.' }
+};
+
+export default viewList;
